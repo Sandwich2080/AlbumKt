4f2e4f8dbcdc7c665ef9fc504551e7fb03b594f7
Sandwich2080 <androidsandwich@163.com>
2019-10-22 18:12:10 +0800

    [Added] Add a tab for home page.

--- app/src/main/java/com/example/albumkt/MainActivity.kt
+++ app/src/main/java/com/example/albumkt/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.albumkt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.TableLayout
+import butterknife.BindView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity() {
+
+
+    @BindView(R.id.container) lateinit var container:FrameLayout
+    @BindView(R.id.tl_bottom) lateinit var tlBottom:TableLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*val tvHello: TextView = findViewById(R.id.tv_hello)
+        tvHello.setOnClickListener {
+                Log.d("MainActivity", "")
+                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        }*/
+
     }
 }
